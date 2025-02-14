ff11710614b78ae5e82eb15c50415ee538172ded
Gowtham <your.email@example.com>
2025-02-14 22:58:14 +0530

    Feat:client io connection

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ class SocketService{
     private _io:Server
     constructor(){
         console.log("Init socket.io services..")
-        this._io= new Server()
+        this._io= new Server({
+            cors:{
+                allowedHeaders:["*"],
+                origin:"*"
+            },
+        })
     }
     get io(){
         return this._io
@@ -12,12 +17,13 @@ class SocketService{
     public InitailListeners(){
         const io=this.io
         console.log("Init Socket Listeners....")
-
+        this._io=new Server()
         io.on('connect',socket=>{
             console.log("New Socket Connected",socket.id)
-            
+           
             socket.on("event:message",async ({message}:{message:String})=>{
                 console.log("New message Rec.",message)
+                //publish the message to Redis
             })
         })
     }
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,6 +1,6 @@
-'use client'
-import React, {  useCallback,useEffect } from 'react'
-import { io } from 'socket.io-client'
+ 'use client'
+import React, {  useCallback,useContext,useEffect, useState } from 'react'
+import { io,Socket } from 'socket.io-client'
 interface SocketProviderProps{
     children?:React.ReactNode
 }
@@ -10,15 +10,31 @@ interface ISocketContext{
 const SocketContext=React.createContext<ISocketContext|null>(null)
 
 export const SocketProvider:React.FC<SocketProviderProps>=({children})=>{
+    const [socket,setSocket]=useState<Socket>()
+
     const sendMessage:ISocketContext["sendMessage"]=useCallback((msg)=>{
+        if(socket){
+            socket.emit("event:message",{message:msg})
+        }
         console.log("Send Message",msg)
-    },[])
+    },[socket])
     useEffect(()=>{
         const  _socket=io("http://localhost:3005")
-
-        return ()=>{_socket.disconnect()} 
+        setSocket(_socket)
+        return ()=>{
+            _socket.disconnect()
+            setSocket(undefined)
+        } 
     },[])
     return <SocketContext.Provider value={{sendMessage}}>
         {children}
     </SocketContext.Provider>
+}
+
+export const useSocket=()=>{
+    const context=useContext(SocketContext)
+    if(!context){
+        throw new Error("Context must be used inside the Provider")
+    }
+    return context
 }
